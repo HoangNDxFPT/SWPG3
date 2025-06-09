43c7a0f4abaaf45fad90166d657df03431b68943
HoangNDxFPT <hoangnguyenduc674@gmail.com>
2025-06-09 19:29:43 +0700

    sửa chữ grug file test

--- BE/src/test/java/com/example/druguseprevention/DugUsePreventationApplicationTests.java
+++ BE/src/test/java/com/example/druguseprevention/DugUsePreventationApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DugUsePreventationApplicationTests {
+class DrugUsePreventationApplicationTests {
 
 	@Test
 	void contextLoads() {
